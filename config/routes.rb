215e74639edade9972d35ae0017407ab2fd22e9f
Mila Zangirolame <milazangirolame@gmail.com>
2017-11-06 17:05:19 -0200

    longest word game

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'user', to: 'games#user'
+
+  post 'score', to: 'games#score'
+
+  root to: 'games#user'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
